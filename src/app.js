8d4172cf3354224da3e92061557411efbcc9cb9c
shutingcasey <shutingcasey@gmail.com>
2026-05-18 18:58:22 -0400

    Finish Lab 2 - Test Authentication Flows

---
+++ src/app.js
@@ -0,0 +1,33 @@
+// src/app.js
+
+import { signIn, getUser } from './auth';
+
+async function init() {
+  // Get our UI elements
+  const userSection = document.querySelector('#user');
+  const loginBtn = document.querySelector('#login');
+
+  // Wire up event handlers to deal with login and logout.
+  loginBtn.onclick = () => {
+    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+    signIn();
+  };
+
+  // See if we're signed in (i.e., we'll have a `user` object)
+  const user = await getUser();
+  if (!user) {
+    return;
+  }
+
+  // Update the UI to welcome the user
+  userSection.hidden = false;
+
+  // Show the user's username
+  userSection.querySelector('.username').innerText = user.username;
+
+  // Disable the Login button
+  loginBtn.disabled = true;
+}
+
+// Wait for the DOM to be ready, then start the app
+addEventListener('DOMContentLoaded', init);
