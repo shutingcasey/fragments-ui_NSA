dce2aed490b09acb03f84232790de685208359d2
shutingcasey <shutingcasey@gmail.com>
2026-05-18 20:30:55 -0400

    Connect Client Web App to Secure Microservice

---
+++ src/api.js
@@ -0,0 +1,30 @@
+// src/api.js
+
+// fragments microservice API to use, defaults to localhost:8080 if not set in env
+const apiUrl = process.env.API_URL || 'http://localhost:8080';
+
+/**
+ * Given an authenticated user, request all fragments for this user from the
+ * fragments microservice (currently only running locally). We expect a user
+ * to have an `idToken` attached, so we can send that along with the request.
+ */
+export async function getUserFragments(user) {
+  console.log('Requesting user fragments data...');
+  try {
+    const fragmentsUrl = new URL('/v1/fragments', apiUrl);
+    const res = await fetch(fragmentsUrl, {
+      // Generate headers with the proper Authorization bearer token to pass.
+      // We are using the `authorizationHeaders()` helper method we defined
+      // earlier, to automatically attach the user's ID token.
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Successfully got user fragments data', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragments', { err });
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // src/app.js
 
 import { signIn, getUser } from './auth';
+import { getUserFragments } from './api';
 
 async function init() {
   // Get our UI elements
@@ -18,6 +19,9 @@ async function init() {
   if (!user) {
     return;
   }
+
+  // Do an authenticated request to the fragments API server and log the result
+  const userFragments = await getUserFragments(user);
 
   // Update the UI to welcome the user
   userSection.hidden = false;
